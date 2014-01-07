293a94c6cfb92598eb126f9fc6d2369ccf361618
Claus Bechtel <tpt@piketec.com>
2014-01-07 14:13:51 +0100

    CR 10017: Es fehlt ein clean vor dem Build, deswegen enthielt die hpi-Datei class-Leichen. Der Jenkins hatte das Plugin zwar geladen, aber im Log einige unschöne Meldungen hinterlassen.

--- build.bat
+++ build.bat
@@ -2,9 +2,9 @@ SETLOCAL
 
 : install maven nach c:\tools\Maven
 
-set M2_HOME=c:\tools\maven
+set M2_HOME=C:\tools\maven
 set M2=%M2_HOME%\bin
-call %M2%\mvn package -DskipTests %*
+call %M2%\mvn clean package -DskipTests %*
 
 echo "Build is done."
 
