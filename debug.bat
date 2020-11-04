3386c5a840f80a77478f7a1c937e07c4001799a5
Joachim Kuhnert <joachim.kuhnert@piketec.com>
2020-11-04 17:37:46 +0100

    Updated Eclipse Project

--- debug.bat
+++ debug.bat
@@ -1,3 +1,4 @@
+set JAVA_HOME=C:\Tools\Java\jdk1.8.0_144
 set JENKINS_HOME=C:\tools\Jenkins
 set MAVEN_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n
 mvn -Dmaven.javadoc.skip=true hpi:run
